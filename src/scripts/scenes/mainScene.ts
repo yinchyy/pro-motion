7f054c961a7af2aa1394a518436a95e01d4c70c2
Szymon Studniarz <szymek1019@gmail.com>
2023-01-23 10:51:58 +0100

    change shooting angle with mouse movement

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -10,10 +10,7 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.fpsText = new FpsText(this)
-    //missile = this.matter.add.image(0, 600, 'phaser-logo').setBounce(0.3)
-    //this.matter.add.mouseSpring({ length: 1, stiffness: 0.6 })
     this.matter.world.setBounds()
-    //missile.setFriction(3)
     var cannonHead = this.add.image(60, 621, 'cannon_head').setDepth(1)
     var cannon = this.add.image(60, 669, 'cannon_body').setDepth(1)
     var angle = 0
@@ -23,7 +20,7 @@ export default class MainScene extends Phaser.Scene {
     this.input.on(
       'pointermove',
       function (pointer) {
-        angle = Phaser.Math.Angle.BetweenPoints(cannon, pointer)
+        angle = Phaser.Math.Angle.BetweenPoints(cannonHead, pointer)
         cannonHead.rotation = angle
         Phaser.Geom.Line.SetToAngle(line, cannon.x, cannon.y - 50, angle, 128)
         gfx.clear().strokeLineShape(line)
@@ -32,9 +29,11 @@ export default class MainScene extends Phaser.Scene {
     )
 
     this.input.on('pointerdown', pointer => {
-      this.matter.setVelocity(this.matter.add.image(200, 300, 'pangball').setCircle(15), 30, -55)
-      //this.matter.setVelocity(this.matter.add.circle(0, 600, 25), 30, -55)
-      //this.matter.setVelocity(missile, 30, -55)
+      this.matter.setVelocity(
+        this.matter.add.image(40, 630, 'pangball').setCircle(15),
+        Math.cos(angle) * 50,
+        Math.sin(angle) * 50
+      )
     })
 
     // display the Phaser.VERSION
