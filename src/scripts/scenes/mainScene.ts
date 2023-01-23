f2114486dbf3b481f5f0aa76f342af446d0c6e76
Szymon Studniarz <szymek1019@gmail.com>
2023-01-23 21:17:04 +0100

    draw bezier curve to show missile path (wip)

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -16,6 +16,11 @@ export default class MainScene extends Phaser.Scene {
     var angle = 0
     var gfx = this.add.graphics().setDefaultStyles({ lineStyle: { width: 10, color: 0xffdd00, alpha: 0.5 } })
     var line = new Phaser.Geom.Line()
+    var curve = new Phaser.Curves.QuadraticBezier(
+      new Phaser.Math.Vector2(60, 620),
+      new Phaser.Math.Vector2(150, 350),
+      new Phaser.Math.Vector2(200, 700)
+    )
 
     this.input.on(
       'pointermove',
@@ -23,17 +28,45 @@ export default class MainScene extends Phaser.Scene {
         angle = Phaser.Math.Angle.BetweenPoints(cannonHead, pointer)
         cannonHead.rotation = angle
         Phaser.Geom.Line.SetToAngle(line, cannon.x, cannon.y - 50, angle, 128)
-        gfx.clear().strokeLineShape(line)
+        //gfx.clear().strokeLineShape(line)
+        let p1x = -((Math.pow(50, 2) / 9.81) * Math.sin(2 * angle)) * 3.17
+        console.log(Phaser.Math.RadToDeg(angle))
+        let p1y = 620 - (3.17 * Math.pow(Math.sin(angle) * 50, 2)) / (2 * 9.81)
+        console.log(`y: ${Math.pow(Math.sin(angle) * 50, 2) / (2 * 9.81)}, calculated: ${p1y}`)
+        curve.p1 = new Phaser.Math.Vector2(p1x / 2, p1y)
+        //   curve.p1 = new Phaser.Math.Vector2(
+        //     -(60 + (Math.pow(50, 2) / 9.81) * Math.sin(2 * angle)) / 2,
+        //     100 + Math.pow(50 * Math.sin(angle), 2) / (2 * 9.81)
+        //   )
+        // curve.p1 = new Phaser.Math.Vector2(
+        //   p1x / 2,
+        //   (Math.tan(angle) - ((p1x / 2 / (2 * 50 * Math.cos(angle))) * (p1x / 2 - 60) - 100)) /
+        //     Phaser.Math.RadToDeg(angle)
+        // )
+        //curve.p2 = new Phaser.Math.Vector2(-(Math.pow(50, 2) / 9.81) * Math.sin(2 * angle), 700)
+        curve.p2 = new Phaser.Math.Vector2(p1x, 620)
+        gfx.clear()
+        curve.draw(gfx)
       },
       this
     )
 
     this.input.on('pointerdown', pointer => {
-      this.matter.setVelocity(
-        this.matter.add.image(40, 630, 'pangball').setCircle(15),
-        Math.cos(angle) * 50,
-        Math.sin(angle) * 50
+      let ball = this.matter.add.image(60, 620, 'pangball')
+      ball.setCircle(15)
+      this.matter.setVelocity(ball, Math.cos(angle) * 50, Math.sin(angle) * 50)
+      console.log(
+        `x:${Math.cos(angle) * 50}, y:${Math.sin(angle) * 50}, distance: ${
+          -(Math.sin(2 * angle) * Math.pow(50, 2)) / 9.81
+        }, phaser distance:${Phaser.Math.Distance.BetweenPoints(cannonHead, ball)}`
       )
+      //  let ball = this.matter.add.image(40, 630, 'pangball')
+      //  ball.setCircle(15)
+      //  ball.setMass(10)
+      //  this.matter.applyForce(ball, {
+      //    x: Math.cos(angle) * 3,
+      //    y: Math.sin(angle) * 3
+      //  })
     })
 
     // display the Phaser.VERSION
