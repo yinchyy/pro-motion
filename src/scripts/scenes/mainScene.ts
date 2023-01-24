ceadec202c82f0c93d1643ec260b1d68e5707754
Szymon Studniarz <szymek1019@gmail.com>
2023-01-24 12:44:38 +0100

    refresh values dependant on velocity, draw trajectory on scroll event

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -18,48 +18,54 @@ export default class MainScene extends Phaser.Scene {
     var sketch = this.add.graphics().setDefaultStyles({ lineStyle: { width: 6, color: 0x8fa3a5 } })
     var line = new Phaser.Geom.Line()
     let mToPxRatio = 1.7
+    const drawTrajectory = () => {
+      sketch.clear()
+      const pos = (x: number) => {
+        return (
+          mToPxRatio * x * Math.tan(angle) -
+          (-9.81 / (2 * Math.pow(Math.cos(angle) * data.velocity, 2))) * Math.pow(x, 2)
+        )
+      }
+      if (angle > 1.25 && angle < 1.85) {
+        return
+      }
+      if (angle >= -1.565 && angle <= 1.25) {
+        for (let x = 0; x < 490; x += 3) {
+          if (pos(x) + 620 >= 720) {
+            break
+          }
+          line.setTo(mToPxRatio * x + 60, pos(x) + 620, mToPxRatio * (x + 2) + 60, pos(x + 2) + 620)
+          sketch.strokeLineShape(line)
+        }
+      } else {
+        for (let x = 0; x > -490; x -= 3) {
+          if (pos(x) + 620 >= 720) {
+            break
+          }
+          line.setTo(mToPxRatio * x + 60, pos(x) + 620, mToPxRatio * (x + 2) + 60, pos(x + 2) + 620)
+          sketch.strokeLineShape(line)
+        }
+      }
+    }
+    const refreshDataValues = () => {
+      data.velocity_y = Math.sin(angle) * data.velocity
+      data.velocity_x = Math.cos(angle) * data.velocity
+      data.range = `${(-(Math.pow(data.velocity, 2) / 9.81) * Math.sin(2 * angle)).toFixed(2)}m`
+      if (Phaser.Math.RadToDeg(-angle) < 0) {
+        data.peak = `0.00m`
+      } else {
+        data.peak = `${(Math.pow(data.velocity_y, 2) / (2 * 9.81)).toFixed(2)}m`
+      }
+    }
 
     this.input.on(
       'pointermove',
       function (pointer) {
         angle = Phaser.Math.Angle.BetweenPoints(cannonHead, pointer)
         data.angle = `${Phaser.Math.RadToDeg(-angle).toFixed(2)}deg`
-        data.velocity_y = Math.sin(angle) * data.velocity
-        data.velocity_x = Math.cos(angle) * data.velocity
-        data.range = `${(-(Math.pow(data.velocity, 2) / 9.81) * Math.sin(2 * angle)).toFixed(2)}m`
-        if (Phaser.Math.RadToDeg(-angle) < 0) {
-          data.peak = `0.00m`
-        } else {
-          data.peak = `${(Math.pow(data.velocity_y, 2) / (2 * 9.81)).toFixed(2)}m`
-        }
         cannonHead.rotation = angle
-        sketch.clear()
-        const pos = (x: number) => {
-          return (
-            mToPxRatio * x * Math.tan(angle) -
-            (-9.81 / (2 * Math.pow(Math.cos(angle) * data.velocity, 2))) * Math.pow(x, 2)
-          )
-        }
-        if (angle > 1.25 && angle < 1.85) {
-          return
-        }
-        if (angle >= -1.565 && angle <= 1.25) {
-          for (let x = 0; x < 490; x += 3) {
-            if (pos(x) + 620 >= 720) {
-              break
-            }
-            line.setTo(mToPxRatio * x + 60, pos(x) + 620, mToPxRatio * (x + 2) + 60, pos(x + 2) + 620)
-            sketch.strokeLineShape(line)
-          }
-        } else {
-          for (let x = 0; x > -490; x -= 3) {
-            if (pos(x) + 620 >= 720) {
-              break
-            }
-            line.setTo(mToPxRatio * x + 60, pos(x) + 620, mToPxRatio * (x + 2) + 60, pos(x + 2) + 620)
-            sketch.strokeLineShape(line)
-          }
-        }
+        refreshDataValues()
+        drawTrajectory()
       },
       this
     )
@@ -83,6 +89,8 @@ export default class MainScene extends Phaser.Scene {
       if (event.deltaY > 0) {
         data.velocity -= 2
       }
+      refreshDataValues()
+      drawTrajectory()
     })
 
     var gui = new dat.GUI()
