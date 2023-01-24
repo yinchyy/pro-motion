7ac40385049b690b855a9ab40e2df875f1ef9489
Szymon Studniarz <szymek1019@gmail.com>
2023-01-24 10:44:10 +0100

    draw missile trajectory with lines instead of bezier

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -14,39 +14,24 @@ export default class MainScene extends Phaser.Scene {
     var cannonHead = this.add.image(60, 621, 'cannon_head').setDepth(1)
     var cannon = this.add.image(60, 669, 'cannon_body').setDepth(1)
     var angle = 0
-    var gfx = this.add.graphics().setDefaultStyles({ lineStyle: { width: 10, color: 0xffdd00, alpha: 0.5 } })
+    var sketch = this.add.graphics().setDefaultStyles({ lineStyle: { width: 2, color: 0x00ff00 } })
     var line = new Phaser.Geom.Line()
-    var curve = new Phaser.Curves.QuadraticBezier(
-      new Phaser.Math.Vector2(60, 620),
-      new Phaser.Math.Vector2(150, 350),
-      new Phaser.Math.Vector2(200, 700)
-    )
+    let mToPxRatio = 1.7
 
     this.input.on(
       'pointermove',
       function (pointer) {
         angle = Phaser.Math.Angle.BetweenPoints(cannonHead, pointer)
         cannonHead.rotation = angle
-        Phaser.Geom.Line.SetToAngle(line, cannon.x, cannon.y - 50, angle, 128)
-        //gfx.clear().strokeLineShape(line)
-        let p1x = -((Math.pow(50, 2) / 9.81) * Math.sin(2 * angle)) * 3.17
-        console.log(Phaser.Math.RadToDeg(angle))
-        let p1y = 620 - (3.17 * Math.pow(Math.sin(angle) * 50, 2)) / (2 * 9.81)
-        console.log(`y: ${Math.pow(Math.sin(angle) * 50, 2) / (2 * 9.81)}, calculated: ${p1y}`)
-        curve.p1 = new Phaser.Math.Vector2(p1x / 2, p1y)
-        //   curve.p1 = new Phaser.Math.Vector2(
-        //     -(60 + (Math.pow(50, 2) / 9.81) * Math.sin(2 * angle)) / 2,
-        //     100 + Math.pow(50 * Math.sin(angle), 2) / (2 * 9.81)
-        //   )
-        // curve.p1 = new Phaser.Math.Vector2(
-        //   p1x / 2,
-        //   (Math.tan(angle) - ((p1x / 2 / (2 * 50 * Math.cos(angle))) * (p1x / 2 - 60) - 100)) /
-        //     Phaser.Math.RadToDeg(angle)
-        // )
-        //curve.p2 = new Phaser.Math.Vector2(-(Math.pow(50, 2) / 9.81) * Math.sin(2 * angle), 700)
-        curve.p2 = new Phaser.Math.Vector2(p1x, 620)
-        gfx.clear()
-        curve.draw(gfx)
+        sketch.clear()
+        const pos = (x: number) => {
+          return mToPxRatio * x * Math.tan(angle) - (-9.81 / (2 * Math.pow(Math.cos(angle) * 50, 2))) * Math.pow(x, 2)
+        }
+        for (let x = 0; x < 490; x += 3) {
+          console.log(`pos(${Phaser.Math.RadToDeg(angle)})=${pos(x) + 620}`)
+          line.setTo(mToPxRatio * x + 60, pos(x) + 620, mToPxRatio * (x + 2) + 60, pos(x + 2) + 620)
+          sketch.strokeLineShape(line)
+        }
       },
       this
     )
